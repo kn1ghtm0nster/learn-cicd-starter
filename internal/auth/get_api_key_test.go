26fa5500496313da00c5704835c8eb910d1ff79f
Diego <84253872+kn1ghtm0nster@users.noreply.github.com>
2026-03-04 18:28:04 -0500

    README Update and Unit Tests (#1)
    * CH1-L4: update README
    
    * CH1-L5: add broken workflow
    
    * CH1-L6: update ci workflow to run go version
    
    * CH1-L6: update name of job because can't read
    
    * CH2-L1: add unit tests for GetAPIKey func
    
    * CH2-L2: add unit test step for workflow jobs
    
    * CH2-L2: breaking test suite for ci checks
    
    * CH2-L2: fix broken unit test hehe
    
    * CH2-L3: add coverage flag to unit test ci
    
    * CH2-L4: update README | add ci badge
    
    ---------
    
    Co-authored-by: kn1ghtm0nster <diego.quintanilla@proton.me>

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKeySuccess(t *testing.T) {
+	header := http.Header{}
+	header.Set("Authorization", "ApiKey super-secret-key")
+
+	result, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if result != "super-secret-key" {
+		t.Fatalf("expected 'super-secret-key', got %s", result)
+	}
+}
+
+func TestGetAPIKeyNoHeader(t *testing.T) {
+	headers := http.Header{}
+
+	_, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
+	}
+}
+
+func TestGetAPIKeyInvalidHeader(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "InvalidHeaderValue")
+
+	result, err := GetAPIKey(headers)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	if result != "" {
+		t.Fatalf("expected empty result, got %s", result)
+	}
+
+	if err.Error() != "malformed authorization header" {
+		t.Fatalf("expected error 'malformed authorization header', got %v", err)
+	}
+}
